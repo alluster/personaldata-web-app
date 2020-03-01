667c0ca8aa7befc6f9ae8839fa9c853cbb1938f4
Aleksanteri Heliövaara <aleksanteriheliovaara@Aleksanteris-MacBook-Pro.local>
2020-03-01 19:12:49 +0200

    updates

--- server.js
+++ server.js
@@ -9,120 +9,7 @@ const PORT = process.env.PORT || 3000;
 var nodemailer = require('nodemailer');
 var bodyParser = require('body-parser')
 sgTransport = require('nodemailer-sendgrid-transport');
-
-const userEmailOptions = {
-  title: "Kiitos kun käytit palveluamme!",
-  ingress: "Olemme lähettäneet tietosi yritysrekisteriimme. Tarkkaile sähköpostiasi mahdollisten yhteydenottojen varalta.",
-  body: "Oletamme yritysten lähettävän sinulle sähköpostiosoitteeseesi liittyvät datat. Emme kuitenkaan voi taata että näin käy joten olethan kärsivällinen. Mikäli yrityksistä ei kuulu, olethan kiltti ja annat meille palautetta niin tarkistamme yrityksen tilanteen. ",
-}
-
-const companyEmailOptions = {
-  title: "Hei yrityksellenne on tehty tietopyyntö",
-  ingress: "Olkaa hyvä ja lähettäkää kaikki tähän email osoitteeseen liittyvä GDPR mukainen data tähän sähköpostiin.",
-  body: "Kiitos paljon.",
-}
-
-const companiesEmailList = [
-	"aleksanteri.heliovaara@helsinki.fi"
-
-]
-const emailToCompanies = `
-<html>
-<head>
-<style>
-background {
-  width: 100%;
-  height: 100%;
-  padding: 30px;
-  }
-h1 {
-	font-family: Montserrat;
-  font-size: 300%;
-}
-  h3 {
-	 font-size: 220%;
-
-  }
-p  {
-  font-family: Montserrat;
-  font-size: 160%;
-  margin-bottom: 20px;
-}
-
-</style>
-</head>
-	<body>
-	<div style="padding: 30px;width: 100%;height:100%">
-	<div>
-		<img style="max-width: 200px" src="https://personaldata.herokuapp.com/logo-dark.png"  alt="Personaldata.fi"/>
-	</div>
-	<div style="max-width: 80%;">
-		<h1>${companyEmailOptions.title}</h1>
-		<h3>${companyEmailOptions.ingress}</h3>
-		<p>${companyEmailOptions.body}</p>
-	</div>
-
-	<div>
-		<a style=" font-size: 14px; text-decoration: underline" href="/http://personaldata.fi">Palaa sivustolle</></a>
-	</div>
-	<div>
-		<a style=" font-size: 14px; text-decoration: underline" href="/https://shop.spreadshirt.fi/personaldatafi/">Tue projektia ja hanki huppari</a>
-	</div>
-	<p>
-	data@personaldata.fi
-	</p>
-	</div>
-	</body>
-</html>`
-
-const emailToUser = `
-<html>
-<head>
-<style>
-background {
-  width: 100%;
-  height: 100%;
-  padding: 30px;
-  }
-h1 {
-	font-family: Montserrat;
-  font-size: 300%;
-}
-  h3 {
-	 font-size: 220%;
-
-  }
-p  {
-  font-family: Montserrat;
-  font-size: 160%;
-  margin-bottom: 20px;
-}
-
-</style>
-</head>
-	<body>
-	<div style="padding: 30px;width: 100%;height:100%">
-	<div>
-		<img style="max-width: 200px" src="https://personaldata.herokuapp.com/logo-dark.png"  alt="Personaldata.fi"/>
-	</div>
-	<div style="max-width: 80%;">
-		<h1>${userEmailOptions.title}</h1>
-		<h3>${userEmailOptions.ingress}</h3>
-		<p>${userEmailOptions.body}</p>
-	</div>
-
-	<div>
-		<a style=" font-size: 14px; text-decoration: underline" href="/http://personaldata.fi">Palaa sivustolle</></a>
-	</div>
-	<div>
-		<a style=" font-size: 14px; text-decoration: underline" href="/https://shop.spreadshirt.fi/personaldatafi/">Tue projektia ja hanki huppari</a>
-	</div>
-	<p>
-	data@personaldata.fi
-	</p>
-	</div>
-	</body>
-</html>`
+const companiesEmailList = require('./companies.json')
 
 
 app.prepare().then(() => {
@@ -138,10 +25,113 @@ app.prepare().then(() => {
 			auth: {
 				api_key: process.env.ADMIN_EMAIL_API_KEY 
 			}
-        }));
-        
+		}));
+		const userEmailOptions = {
+			title: "Kiitos kun käytit palveluamme!",
+			ingress: "Olemme lähettäneet tietosi yritysrekisteriimme. Tarkkaile sähköpostiasi mahdollisten yhteydenottojen varalta.",
+			body: "Oletamme yritysten lähettävän sinulle sähköpostiosoitteeseesi liittyvät datat. Emme kuitenkaan voi taata että näin käy joten olethan kärsivällinen. Mikäli yrityksistä ei kuulu, olethan kiltti ja annat meille palautetta niin tarkistamme yrityksen tilanteen. ",
+		}
+  
+        const companyEmailOptions = {
+			title: "Henkilökohtaiseen dataan liittyvä tietopyyntö",
+			ingress: "Olkaa hyvä ja lähettäkää kaikki email osoitteeseen "+ `${req.body.email}` + " liittyvä data vastauksena tähän sähköpostiin tai uutena viestinä suoraan email osoitteeseen " + `${req.body.email}`,
+			body: "Olette vastaanottaneet tämän viestin koska käyttäjämme on tehnyt tietopyynnön liittyen dataan joka liittyy sähköpostiosoitteeseen " + `${req.body.email}` + " liittyvä data vastauksena tähän sähköpostiin tai uutena viestinä suoraan email osoitteeseen " + `${req.body.email}`,
+		  }
+		const emailToCompanies =
+			`<html>
+				<head>
+				<style>
+				background {
+				width: 100%;
+				height: 100%;
+				padding: 30px;
+				}
+				h1 {
+					font-family: Montserrat;
+				font-size: 300%;
+				}
+				h3 {
+					font-size: 220%;
+				
+				}
+				p  {
+				font-family: Montserrat;
+				font-size: 160%;
+				margin-bottom: 20px;
+				}
+				
+				</style>
+				</head>
+					<body>
+					<div style="padding: 30px;width: 100%;height:100%">
+					<div>
+						<img style="max-width: 200px" src="https://personaldata.herokuapp.com/logo-dark.png"  alt="Personaldata.fi"/>
+					</div>
+					<div style="max-width: 80%;">
+						<h1>${companyEmailOptions.title}</h1>
+						<h3>${companyEmailOptions.ingress}</h3>
+						<p>${companyEmailOptions.body}</p>
+					</div>
+				
+					<p>
+						Lisätietoa: data@personaldata.fi
+					</p>
+					</div>
+					</body>
+				</html>
+				`
+		const emailToUser = `
+			<html>
+			<head>
+			<style>
+			background {
+			width: 100%;
+			height: 100%;
+			padding: 30px;
+			}
+			h1 {
+				font-family: Montserrat;
+			font-size: 300%;
+			}
+			h3 {
+				font-size: 220%;
+
+			}
+			p  {
+			font-family: Montserrat;
+			font-size: 160%;
+			margin-bottom: 20px;
+			}
+
+			</style>
+			</head>
+				<body>
+				<div style="padding: 30px;width: 100%;height:100%">
+				<div>
+				<img style="max-width: 200px" src="https://personaldata.herokuapp.com/logo-dark.png"  alt="Personaldata.fi"/>
+				</div>
+				<div style="max-width: 80%;">
+					<h1>${userEmailOptions.title}</h1>
+					<h3>${userEmailOptions.ingress}</h3>
+					<p>${userEmailOptions.body}</p>
+				</div>
+
+				<div>
+					<a  href="/http://personaldata.fi">www.personaldata.fi</></a>
+				</div>
+				<div>
+					<a href="/https://shop.spreadshirt.fi/personaldatafi/">Tue projektia hankkimalla huppari verkkokaupastamme</a>
+				</div>
+				<p>
+				data@personaldata.fi
+				</p>
+				</div>
+				</body>
+			</html>`
+
+
         var mailToUser = {
-			from: 'data@personaldata.fi',
+			from: `${req.body.email}`,
 			to: `${req.body.email}`,
 			subject: `${userEmailOptions.title}`,
 			text: `${userEmailOptions.ingress}`,
